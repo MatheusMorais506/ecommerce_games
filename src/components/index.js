71153dd0a7a3ec19024a8d9f11e4e03e7213228d
MatheusMorais506 <matheus.moraes09@hotmail.com>
2021-10-06 21:44:30 -0300

    feat(index.js): add file controller index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Products } from './Products/Products';
+export { default as ShoppingCart} from './Cart/ShoppingCart';
+export { default as Footer} from './Footer/Footer';
